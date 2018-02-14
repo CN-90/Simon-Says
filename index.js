5cc1dc2cef237039248877d6e19dde4be2384149
CN90 <chr.revel@gmail.com>
2018-02-13 18:13:11 -0700

    Set up event listeners & worked on HTML

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 let playerSequence = [];
 let randomSequence = [];
+let roundNumber = 1;
+let gameOver = false;
 
 const redSquare = document.querySelector('.red');
 const blueSquare = document.querySelector('.blue');
 const greenSquare = document.querySelector('.green');
 const yellowSquare = document.querySelector('.yellow')
 
-redSquare.addEventListener('click', function(){
-  console.log(this.dataset.color);
-  playerSequence.push(this.dataset.color);
-  console.log(playerSequence)
-  console.log(playerSequence[0] == randomSequence[0]);
-})
+setEventListeners()
 
+
+function setEventListeners(){
+  let squares = [ redSquare, blueSquare, greenSquare, yellowSquare]
+  for(let i = 0; i < squares.length; i++){
+    squares[i].addEventListener('click', function(){
+      playerSequence.push(this.dataset.color);
+      console.log(checkSequence());
+    })
+  }
+}
+
+function startGame(){
+  clearInterval(myInterval)
+  gameOver = false;
+  while(!gameOver){
+    addToSequence();
+    console.log(randomSequence)
+    if(randomSequence.length >= 1){
+      gameOver = true;
+    }
+  }
+}
 
 function addToSequence(){
   // Adds next color to random sequence player must follow.
@@ -22,11 +41,12 @@ function addToSequence(){
 }
 
 function checkSequence(){
-  
+  // Checks if the last colors in sequence match.
+  let lastPlayerSequence = playerSequence[playerSequence.length - 1]
+  let lastRandomSequence = randomSequence[randomSequence.length - 1]
+  return lastPlayerSequence === lastRandomSequence;
 }
 
-console.log(randomSequence);
-addToSequence()
-addToSequence()
-addToSequence()
-console.log(randomSequence)
+function nextRound(){
+  playerSequence = []
+}
