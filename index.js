620bfee4c1ebd1609804ef2ffc1f7a064d9abc4f
CN90 <chr.revel@gmail.com>
2018-02-26 08:45:30 -0700

    Added audio & toggle opacity functionality

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 let playerSequence = [];
 let randomSequence = [];
-let roundNumber = 1;
+let roundNumber = 0;
 let gameOver = false;
 
 const redSquare = document.querySelector('.red');
 const blueSquare = document.querySelector('.blue');
 const greenSquare = document.querySelector('.green');
 const yellowSquare = document.querySelector('.yellow')
+
+const redAudio = document.querySelector('#red-audio')
+const blueAudio = document.querySelector('#blue-audio')
+const greenAudio = document.querySelector('#green-audio')
+const yellowAudio = document.querySelector('#yellow-audio')
+
 
 setEventListeners()
 
@@ -16,27 +22,28 @@ function setEventListeners(){
   for(let i = 0; i < squares.length; i++){
     squares[i].addEventListener('click', function(){
       playerSequence.push(this.dataset.color);
+      changeOpacity(this.dataset.color);
+      playSound(this.dataset.color);
       if(!checkSequence()){
+        console.log(playerSequence, randomSequence)
         console.log('Game over!')
       } else if(playerSequence.length === randomSequence.length){
-        nextRound();
+        nextRound(randomSequence[randomSequence.length - 1]);
       }
     })
   }
-}
-
-function startGame(){
-  
 }
 
 function addToSequence(){
   // Adds next color to random sequence player must follow.
   let number = Math.floor(Math.random() * 4);
   let colors = {0: 'red', 1: 'blue', 2: 'yellow', 3: 'green'}
-  randomSequence.push(colors[number])
+  randomSequence.push(colors[number]);
+  return colors[number];
 }
 
 function checkSequence(){
+  // ensures user sequence matches random Sequence.
   for(let i = 0; i < playerSequence.length; i++){
     if(playerSequence[i] !== randomSequence[i]){
       return false
@@ -47,5 +54,42 @@ function checkSequence(){
 
 function nextRound(){
   playerSequence = []
-  addToSequence();
+  let color = addToSequence();
+  timer = 1000;
+  for(let color of randomSequence){
+    setTimeout(() => {
+      playSound(color);
+      changeOpacity(color)
+    }, timer)
+    timer += 1000;
+  }
+  renderRound();
+}
+
+function playSound(color){
+  let soundToPlay = {
+    red: redAudio,
+    blue: blueAudio,
+    yellow: yellowAudio,
+    green: greenAudio
+  }
+  soundToPlay[color].play();
+}
+
+function renderRound(){
+  roundNumber++;
+  document.querySelector('#round').innerHTML = `Round: ${roundNumber}`
+}
+
+function changeOpacity(color){
+  let colors = {
+    red: redSquare,
+    blue: blueSquare,
+    yellow: yellowSquare,
+    green: greenSquare
+  }
+  colors[color].style.opacity = '1';
+  setTimeout(() => {
+    colors[color].style.opacity = '.5';
+  }, 500)
 }
